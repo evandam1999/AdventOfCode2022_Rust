9c1c177aafadc355adc85a7dc4d892c8e6a28062
Erick Van Dam <erick@runsafesecurity.com>
2023-03-30 22:55:36 -0500

    day 5-1 done

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,7 +1,9 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use std::collections::VecDeque;
+use std::iter;
 
+#[derive(Debug)]
 struct amove {
     count: i32,
     from: i32,
@@ -13,50 +15,57 @@ impl amove {
         let data:Vec<&str> = input.split(' ').collect();
         assert_eq!(data.len(), 6, "Error in move input, not 6 items");
         amove { count: data[1].parse::<i32>().unwrap(),
-            from: data[3].parse::<i32>().unwrap(),
-            to: data[5].parse::<i32>().unwrap()}
+            from: data[3].parse::<i32>().unwrap() - 1,
+            to: data[5].parse::<i32>().unwrap() - 1}
     }
 }
 
 #[derive(Debug)]
 struct stack {
-    _1: VecDeque<char>,
-    _2: VecDeque<char>,
-    _3: VecDeque<char>,
-    _4: VecDeque<char>,
-    _5: VecDeque<char>,
-    _6: VecDeque<char>,
-    _7: VecDeque<char>,
-    _8: VecDeque<char>,
-    _9: VecDeque<char>,
+    s: Vec<VecDeque<char>>,
 }
+
 
 impl stack {
     fn add_from_top(&mut self, input: &str) {
         let v:Vec<char> = input.chars().collect();
-        if v[1] != ' ' { self._1.push_front(v[1])};
-        if v[5] != ' ' { self._2.push_front(v[2])};
-        if v[9] != ' ' { self._3.push_front(v[3])};
-        if v[13] != ' ' { self._4.push_front(v[4])};
-        if v[17] != ' ' { self._5.push_front(v[5])};
-        if v[21] != ' ' { self._6.push_front(v[6])};
-        if v[25] != ' ' { self._7.push_front(v[7])};
-        if v[29] != ' ' { self._8.push_front(v[8])};
-        if v[33] != ' ' { self._9.push_front(v[9])};
+        // println!("{:?}", input.len());
+        let len = input.len();
+        if len >= 1 {if v[1] != ' ' { self.s[0].push_front(v[1])}};
+        if len >= 5 {if v[5] != ' ' { self.s[1].push_front(v[5])}};
+        if len >= 9 {if v[9] != ' ' { self.s[2].push_front(v[9])}};
+        if len >= 13 {if v[13] != ' ' { self.s[3].push_front(v[13])}};
+        if len >= 17 {if v[17] != ' ' { self.s[4].push_front(v[17])}};
+        if len >= 21 {if v[21] != ' ' { self.s[5].push_front(v[21])}};
+        if len >= 25 {if v[25] != ' ' { self.s[6].push_front(v[25])}};
+        if len >= 29 {if v[29] != ' ' { self.s[7].push_front(v[29])}};
+        if len >= 33 {if v[33] != ' ' { self.s[8].push_front(v[33])}};
+    }
+
+    //TODO:  Issue is here in that moving 2 should move them as a group not one at a time??
+    fn process_move(&mut self, input: &amove) {
+        let from: usize = input.from as usize;
+        let to: usize = input.to as usize;
+        for i in 0..input.count {
+            let moving = self.s[from].pop_back();
+            self.s[to].push_back(moving.unwrap());
+        }
     }
 }
 
 impl Default for stack {
     fn default() -> Self {
-        stack { _1: VecDeque::new(),
-            _2: VecDeque::new(),
-            _3: VecDeque::new(),
-            _4: VecDeque::new(),
-            _5: VecDeque::new(),
-            _6: VecDeque::new(),
-            _7: VecDeque::new(),
-            _8: VecDeque::new(),
-            _9: VecDeque::new(),
+        // Has to be a better way
+        stack { s: vec![VecDeque::new(),
+            VecDeque::new(),
+            VecDeque::new(),
+            VecDeque::new(),
+            VecDeque::new(),
+            VecDeque::new(),
+            VecDeque::new(),
+            VecDeque::new(),
+            VecDeque::new(),
+            ]
         }
     }
 }
@@ -64,11 +73,29 @@ impl Default for stack {
 fn main() {
     let file = File::open("./input.txt").unwrap();
     let reader = BufReader::new(file);
+    let mut s: stack = stack::default();
+    let mut moves: VecDeque<amove> = VecDeque::new();
 
     for line in reader.lines() {
-        println!("{:?}", line);
+        if line.as_ref().unwrap().contains("[") {
+            println!("{:?}", line);
+            s.add_from_top(&line.unwrap())
+        }
+        else if line.as_ref().unwrap().contains("move") {
+            let a = amove::init(&line.unwrap());
+            moves.push_back(a);
+        }
     }
-    //Start here have amove and stack implemented at least started.
+    // println!("{:?}", s);
+    for m in moves {
+        s.process_move(&m);
+    }
+    println!("{:?}", s);
+    for c in &s.s {
+        println!("{:?}", c.back().unwrap());
+    }
+    println!("{}", "I NEED TO CLEAN THIS OUTPUT UP to just give the answer");
+
 }
 
 #[cfg(test)]
@@ -78,18 +105,28 @@ mod tests {
     fn test_amove_init() {
         let x = amove::init("move 1 from 3 to 9");
         assert_eq!(x.count, 1);
-        assert_eq!(x.from, 3);
-        assert_eq!(x.to, 9);
+        assert_eq!(x.from, 2);
+        assert_eq!(x.to, 8);
     }
 
     #[test]
     fn test_add_from_top() {
         let mut a = stack::default();
         a.add_from_top("[D]                     [N] [F]    ");
-        let c: Vec<&char> = a._1.iter().collect();
+        let c: Vec<&char> = a.s[0].iter().collect();
         // println!("{:?}", a);
         assert_eq!(*c[0], 'D');
-        assert_eq!(a._2.is_empty(), true);
+        assert_eq!(a.s[1].is_empty(), true);
+    }
+
+    #[test]
+    fn test_proc_move() {
+        let mut s = stack::default();
+        s.add_from_top("[D]                     [N] [F]    ");
+        s.add_from_top("[X]                     [N] [F]    ");
+        let a = amove::init("move 2 from 1 to 2");
+        s.process_move(&a);
+        println!("{:?}", s)
     }
 
     #[test]
